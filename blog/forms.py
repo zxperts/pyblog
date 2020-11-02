4e7a3214b87bacc0dc0ff522beb813bf8fb55e98
MUNSI Patrick <patrick.munsi@ethias.be>
2020-11-02 22:28:22 +0100

    Créer un commentaire avec un modelform

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+from blog.models import Comment
+
+class CreateCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['author_name', 'text']
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post,Comment
 from blog import model_helpers
 from blog import navigation
+from blog.forms import CreateCommentForm
 
 
 # Create your views here.
@@ -27,11 +28,23 @@ def post_detail(request, post_id):
         .exclude(pk=post.id)
     comments=post.comments.exclude(status=Comment.STATUS_HIDDEN).order_by('created_at')
 
+    if request.method == 'POST':
+        comment_form = CreateCommentForm(request.POST)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment_form.save()
+
+    else:
+        comment_form=CreateCommentForm()
+
     context = {
         'navigation_items': navigation.navigation_items(navigation.NAV_POSTS),
         'post': post,
         'post_same_category': post_same_category,
-        'comments': comments
+        'comments': comments,
+        'comment_form': comment_form,
     }
 
     return render(request, 'blog/post_detail.html', context)
